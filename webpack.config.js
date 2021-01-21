b2150f4de870d56bafec502db166c91e37072b74
MongieLee <le30n@outlook.com>
2021-01-21 23:46:41 +0800

    添加webpack的externals

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  // mode: 'development',
   mode: 'production',
   entry: {
     index: './lib/index.tsx',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
   output: {
     path: path.resolve(__dirname, 'dist/lib'),
@@ -25,4 +29,18 @@ module.exports = {
       template: 'index.html',
     }),
   ],
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    },
+  },
 };
