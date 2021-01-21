3a0dada22c34fdcfc8d29e8df7b756cc6c27c3dc
MongieLee <le30n@outlook.com>
2021-01-21 23:25:38 +0800

    安装react和react-dom

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,4 +1,2 @@
-console.log('index233333')
-const div = document.createElement('div')
-div.innerHTML = 'test'
-document.body.appendChild(div)
+import React from 'react';
+import ReactDOM from 'react-dom';
